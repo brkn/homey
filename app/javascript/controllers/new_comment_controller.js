41f96767eb339f06dd703070c50af38493e9d62b
Berkan Unal <Berkanunal@gmail.com>
2025-04-11 16:05:06 +0300

    Enable form submitting via cmd+enter

--- app/javascript/controllers/new_comment_controller.js
+++ app/javascript/controllers/new_comment_controller.js
@@ -1,7 +1,21 @@
-// import { Controller } from "stimulus"
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    this.element.addEventListener('keydown', this.handleKeyDown.bind(this))
+  }
+
+  disconnect() {
+    this.element.removeEventListener('keydown', this.handleKeyDown.bind(this))
+  }
+
+  handleKeyDown(event) {
+    if (event.key === 'Enter' && (event.metaKey || event.ctrlKey)) {
+      event.preventDefault()
+      this.element.requestSubmit()
+    }
+  }
+
   reset() {
     this.element.reset()
   }
